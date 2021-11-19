e279fd84c7a7d12c2ba863714dc6b0dd08efe716
Firstname Lastname <your_email@example.com>
2021-11-20 08:30:00 +0900

    jQuery実装
    アコーディオンの実装

--- shogo/middle/js/script.js
+++ shogo/middle/js/script.js
@@ -1,3 +1,13 @@
 $(function() {
-
+    //accordion
+    $('.faq-list').click(function() {
+        var $answer = $(this).find('.answer');
+        if ($answer.hasClass('open')) {
+            $answer.removeClass('open');
+            $answer.slideUp();
+        } else {
+            $answer.addClass('open');
+            $answer.slideDown();
+        }
+    });
 });
